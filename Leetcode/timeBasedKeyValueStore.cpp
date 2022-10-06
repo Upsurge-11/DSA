df1c1815c8a0bb3f7968e170f64258de1268335b
Arghyadeep Mukherjee <arghyadeepbumbum@gmail.com>
2022-10-06 14:35:36 +0530

    .

--- Leetcode/timeBasedKeyValueStore.cpp
+++ Leetcode/timeBasedKeyValueStore.cpp
@@ -2,29 +2,35 @@
 
 using namespace std;
 
-class TimeMap {
+class TimeMap
+{
   map<string, vector<pair<int, string>>> m;
+
 public:
-    TimeMap() {
-        
+  TimeMap()
+  {
+  }
+
+  void set(string key, string value, int timestamp)
+  {
+    m[key].push_back({timestamp, value});
+  }
+
+  string get(string key, int timestamp)
+  {
+    auto it = lower_bound(m[key].begin(), m[key].end(), make_pair(timestamp, string()));
+    if (it == m[key].end() || it->first > timestamp)
+    {
+      if (it == m[key].begin())
+        return "";
+      --it;
     }
-    
-    void set(string key, string value, int timestamp) {
-        m[key].push_back({timestamp, value});
-    }
-    
-    string get(string key, int timestamp) {
-        auto it = lower_bound(m[key].begin(), m[key].end(), make_pair(timestamp, string()));
-        if (it == m[key].end() || it->first > timestamp) {
-            if (it == m[key].begin()) return "";
-            --it;
-        }
-        return it->second;
-    }
+    return it->second;
+  }
 };
 
 int main()
 {
-  
+
   return 0;
 }
