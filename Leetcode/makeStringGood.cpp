133866afe761d774099685da6ab7ef0f37e6bc6c
Upsurge-11 <arghyadeepbumbum@gmail.com>
2022-11-08 16:33:21 +0530

    08/11/2022 Leetcode Daily Challenge.

---
+++ Leetcode/makeStringGood.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string makeGood(string s)
+{
+  int n = s.length();
+
+  for (int i = 1; i < n; i++)
+  {
+    }
+}
+
+int main()
+{
+  string s;
+  cin >> s;
+
+  string ans = makeGood(s);
+
+  cout << ans << "\n";
+
+  return 0;
+}
