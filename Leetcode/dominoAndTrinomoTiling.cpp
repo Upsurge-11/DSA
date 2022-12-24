cb109a1ac17d3d2fd4228e7671755f426a7e95c7
Arghyadeep Mukherjee <arghyadeepbumbum@gmail.com>
2022-12-24 10:36:48 +0530

    24/12/2022 Leetcode Daily Challenge.

---
+++ Leetcode/dominoAndTrinomoTiling.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int numTilings(int n)
+{
+  unsigned int dp[n + 3];
+  dp[0] = 1;
+  dp[1] = 2;
+  dp[2] = 5;
+  for (int i = 3; i < n; i++)
+  {
+    dp[i] = (2 * dp[i - 1] + dp[i - 3]) % 1000000007;
+  }
+  return dp[n - 1];
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  cout << numTilings(n) << "\n";
+
+  return 0;
+}
