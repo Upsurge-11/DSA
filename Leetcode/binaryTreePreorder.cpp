3eb9bc0bffebb7e20b53fb477d0e224bc4f23325
Arghyadeep Mukherjee <arghyadeepbumbum@gmail.com>
2023-01-09 11:18:02 +0530

    09/01/2023 Leetcode Daily Challenge.

---
+++ Leetcode/binaryTreePreorder.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+void util(vector<int> &ans, TreeNode *root)
+{
+  if (root == NULL)
+    return;
+  ans.push_back(root->val);
+  util(ans, root->left);
+  util(ans, root->right);
+}
+
+vector<int> preorderTraversal(TreeNode *root)
+{
+  vector<int> ans;
+  util(ans, root);
+  return ans;
+}
+
+int main()
+{
+
+  return 0;
+}
