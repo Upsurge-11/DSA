3be450025efae4c31146f5a094455d3ebb00265b
Arghyadeep Mukherjee <arghyadeepbumbum@gmail.com>
2023-02-03 17:54:59 +0530

    03/02/2023 Leetcode Daily Challenge.

---
+++ Leetcode/zigzagConversion.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string convert(string s, int numRows)
+{
+  if (numRows == 1)
+    return s;
+  string res;
+  int n = s.size();
+  int cycleLen = 2 * numRows - 2;
+  for (int i = 0; i < numRows; i++)
+  {
+    for (int j = 0; j + i < n; j += cycleLen)
+    {
+      res += s[j + i];
+      if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+        res += s[j + cycleLen - i];
+    }
+  }
+  return res;
+}
+
+int main()
+{
+  string s;
+  cin >> s;
+  int numRows;
+  cin >> numRows;
+  cout << convert(s, numRows) << "\n";
+
+  return 0;
+}
