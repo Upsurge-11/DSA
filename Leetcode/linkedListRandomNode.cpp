8b3ed7cc90ebc5d6d0eb238a4a43d80d3090ea1c
Arghyadeep Mukherjee <arghyadeepbumbum@gmail.com>
2023-03-10 13:01:39 +0530

    10/03/2023 Leetcode Daily Challenge.

---
+++ Leetcode/linkedListRandomNode.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+private:
+  ListNode *head;
+
+public:
+  Solution(ListNode *head)
+  {
+    this->head = head;
+    std::srand(std::time(0));
+  }
+
+  int getRandom()
+  {
+    int count = 0;
+    int result = 0;
+    ListNode *curr = head;
+
+    while (curr)
+    {
+      count++;
+      if (std::rand() % count == 0)
+        result = curr->val;
+      curr = curr->next;
+    }
+    return result;
+  }
+};
+
+int main()
+{
+
+  return 0;
+}
