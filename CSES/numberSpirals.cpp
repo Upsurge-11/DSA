88c79fafe8b0768be1ba94e4c2f42d14ebbd29f9
Upsurge-11 <arghyadeepbumbum@gmail.com>
2023-04-08 11:59:26 +0530

    CSES Number Spirals.

--- CSES/numberSpirals.cpp
+++ CSES/numberSpirals.cpp
@@ -21,6 +21,24 @@ signed main()
   {
     int x, y;
     cin >> y >> x;
+    int z = max(x, y);
+    int z2 = z * z;
+    int ans;
+    if (z % 2 == 0)
+    {
+      if (z == y)
+        ans = z2 - x + 1;
+      else
+        ans = z2 - 2 * z + y + 1;
+    }
+    else
+    {
+      if (z == x)
+        ans = z2 - y + 1;
+      else
+        ans = z2 - 2 * z + x + 1;
+    }
+    cout << ans << "\n";
   }
 
   return 0;
