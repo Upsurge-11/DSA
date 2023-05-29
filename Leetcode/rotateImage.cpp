08bce98b07d9ddb6832f0b2818e27425d603e142
Upsurge-11 <arghyadeepbumbum@gmail.com>
2023-05-29 09:36:50 +0530

    Rotate Image.

--- Leetcode/rotateImage.cpp
+++ Leetcode/rotateImage.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 void rotate(vector<vector<int>> &matrix)
 {
+  int n = matrix.size();
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = i; j < n; j++)
+      swap(matrix[i][j], matrix[j][i]);
+  }
+  for (int i = 0; i < n; i++)
+    reverse(matrix[i].begin(), matrix[i].end());
 }
 
 int main()
@@ -14,17 +22,13 @@ int main()
   for (int i = 0; i < n; i++)
   {
     for (int j = 0; j < n; j++)
-    {
       cin >> matrix[i][j];
-    }
   }
   rotate(matrix);
   for (int i = 0; i < n; i++)
   {
     for (int j = 0; j < n; j++)
-    {
       cout << matrix[i][j] << " ";
-    }
     cout << "\n";
   }
 
