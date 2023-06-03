3232f83e052546b23aa62209f9f9591f52a1b078
Upsurge-11 <arghyadeepbumbum@gmail.com>
2023-06-03 14:08:25 +0530

    Binary Search go brrrr...

--- Leetcode/singleNonDuplicate.cpp
+++ Leetcode/singleNonDuplicate.cpp
@@ -2,26 +2,29 @@
 
 using namespace std;
 
-int singleNonDuplicate(vector<int> &nums)
-{
-  int low = 0;
-  int high = nums.size() - 1;
-  int mid;
-  while (low < high)
-  {
-    mid = (low + high) / 2;
-    if (mid % 2 == 1)
-      mid--;
-    if (nums[mid] == nums[mid + 1])
-      low = mid + 2;
+int singleNonDuplicate(vector<int> &nums) {
+  int n = nums.size();
+  if (n == 1)
+    return nums[0];
+  if (nums[0] != nums[1])
+    return nums[0];
+  if (nums[n - 1] != nums[n - 2])
+    return nums[n - 1];
+  int l = 1, r = n - 2;
+  while (l <= r) {
+    int mid = (l + r) / 2;
+    if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+      return nums[mid];
+    if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) ||
+        (mid % 2 == 0 && nums[mid] == nums[mid + 1]))
+      l = mid + 1;
     else
-      high = mid;
+      r = mid - 1;
   }
-  return nums[low];
+  return -1;
 }
 
-int main()
-{
+int main() {
   int n;
   cin >> n;
   vector<int> nums(n);
