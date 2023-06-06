82f783630921060897796f6134a01afe7ba504ed
Upsurge-11 <arghyadeepbumbum@gmail.com>
2023-06-06 09:08:03 +0530

    06/06/2023 Leetcode Daily Challenge.

---
+++ Leetcode/canMakeApFromSequence.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool canMakeArithmeticProgression(vector<int> &arr) {
+  sort(arr.begin(), arr.end());
+  int diff = arr[1] - arr[0];
+  for (int i = 2; i < arr.size(); i++) {
+    if (arr[i] - arr[i - 1] != diff)
+      return false;
+  }
+  return true;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+  cout << canMakeArithmeticProgression(arr) << "\n";
+  return 0;
+}
