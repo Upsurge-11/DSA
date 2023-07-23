576a54405cd5bef376738f9dfe80cf4f4a3cda15
Upsurge-11 <arghyadeepbumbum@gmail.com>
2023-07-23 18:46:27 +0530

    23/07/2023 Leetcode Daily Challenge.

---
+++ Leetcode/allPossibleFullBinaryTrees.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+vector<TreeNode *> allPossibleFBT(int n)
+{
+  if (n % 2 == 0)
+    return {};
+
+  vector<TreeNode *> list;
+  if (n == 1)
+    list.push_back(new TreeNode(0));
+  else
+  {
+    for (int i = 1; i <= n - 1; i += 2)
+    {
+      vector<TreeNode *> lTrees = allPossibleFBT(i);
+      vector<TreeNode *> rTrees = allPossibleFBT(n - i - 1);
+      for (TreeNode *lt : lTrees)
+      {
+        for (TreeNode *rt : rTrees)
+          list.push_back(new TreeNode(0, lt, rt));
+      }
+    }
+  }
+  return list;
+}
+
+int main()
+{
+
+  return 0;
+}
