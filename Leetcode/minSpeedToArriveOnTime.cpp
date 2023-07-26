dc7c30b2bda7110600515e50b0c76a1c0dcd0bc0
Upsurge-11 <arghyadeepbumbum@gmail.com>
2023-07-26 13:06:13 +0530

    26/07/2023 Leetcode Daily Challenge.

---
+++ Leetcode/minSpeedToArriveOnTime.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int minSpeedOnTime(vector<int> &dist, double hour)
+{
+  int n = dist.size();
+  if (hour <= n - 1)
+    return -1;
+  int l = 1, r = 1e7;
+  while (l < r)
+  {
+    int mid = (l + r) / 2;
+    double time = 0;
+    for (int i = 0; i < n - 1; i++)
+      time += ceil((double)dist[i] / mid);
+    time += (double)dist[n - 1] / mid;
+
+    if (time <= hour)
+      r = mid;
+    else
+      l = mid + 1;
+  }
+  return l;
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  double hour;
+  cin >> hour;
+  vector<int> dist(n);
+  for (int i = 0; i < n; i++)
+    cin >> dist[i];
+  cout << minSpeedOnTime(dist, hour) << "\n";
+
+  return 0;
+}
