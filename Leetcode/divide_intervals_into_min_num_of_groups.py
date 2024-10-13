a991d1540980f36dccec927524968a53ce2d1067
Upsurge-11 <arghyadeepbumbum@gmail.com>
2024-10-13 10:28:42 +0530

    Leetcode Daily Challenge 12th October 2023.

---
+++ Leetcode/divide_intervals_into_min_num_of_groups.py
@@ -0,0 +1,19 @@
+def main():
+  intervals = [list(map(int, input().split())) for _ in range(int(input()))]
+  print(minGroups(intervals))
+
+def minGroups(intervals: list[list[int]]) -> int:
+  start_times = sorted(i[0] for i in intervals)
+  end_times = sorted(i[1] for i in intervals)
+  end_ptr, group_count = 0, 0
+
+  for start in start_times:
+    if start > end_times[end_ptr]:
+      end_ptr += 1
+    else:
+      group_count += 1
+
+  return group_count
+
+if __name__ == '__main__':
+  main()
