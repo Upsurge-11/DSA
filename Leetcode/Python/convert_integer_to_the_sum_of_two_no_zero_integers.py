268d365eccc6793cdd8e90204dafa33e3caabf26
Arghyadeep Mukherjee <arghyadeepbumbum@gmail.com>
2025-09-08 23:22:54 +0530

    Add implementation for converting an integer to the sum of two non-zero integers

---
+++ Leetcode/Python/convert_integer_to_the_sum_of_two_no_zero_integers.py
@@ -0,0 +1,11 @@
+def main():
+  n = int(input())
+
+def getNoZeroIntegers(n: int) -> list[int]:
+  for i in range(1, n):
+    if '0' not in str(i) and '0' not in str(n - i):
+      return [i, n - i]
+  return []
+
+if __name__ == '__main__':
+  main()
