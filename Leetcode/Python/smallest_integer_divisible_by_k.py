0ddfc29a0aed0705ac68ea7da36ab4aceba59d10
Arghyadeep Mukherjee <arghyadeepbumbum@gmail.com>
2025-11-25 19:59:15 +0530

    Add implementation for finding the smallest integer divisible by k

---
+++ Leetcode/Python/smallest_integer_divisible_by_k.py
@@ -0,0 +1,16 @@
+def main():
+  k = int(input())
+  print(smallestRepunitDivByK(k))
+
+def smallestRepunitDivByK(k: int) -> int:
+  if k % 2 == 0 or k % 5 == 0:
+    return -1
+  remainder = 0
+  for length in range(1, k + 1):
+    remainder = ((remainder * 10) + 1) % k
+    if remainder == 0:
+      return length
+  return -1
+
+if __name__ == '__main__':
+  main()
