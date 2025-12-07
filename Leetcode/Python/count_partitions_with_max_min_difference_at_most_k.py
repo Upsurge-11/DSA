9f4b31cac0ca979ae838508c244ad4aefdd91f61
Arghyadeep Mukherjee <arghyadeepbumbum@gmail.com>
2025-12-07 16:51:22 +0530

    Implement countPartitions function to calculate the number of valid partitions with max-min difference at most k using dynamic programming and sorted list for efficient range management.

---
+++ Leetcode/Python/count_partitions_with_max_min_difference_at_most_k.py
@@ -0,0 +1,28 @@
+from sortedcontainers import SortedList
+
+def main():
+  nums = list(map(int, input().split()))
+  k = int(input())
+  print(countPartitions(nums, k))
+
+def countPartitions(nums: list[int], k: int) -> int:
+  n = len(nums)
+  mod = 10**9 + 7
+  dp = [0] * (n + 1)
+  prefix = [0] * (n + 1)
+  cnt = SortedList()
+  dp[0] = 1
+  prefix[0] = 1
+  j = 0
+  for i in range(n):
+    cnt.add(nums[i])
+    while j <= i and cnt[-1] - cnt[0] > k:
+      cnt.remove(nums[j])
+      j += 1
+    dp[i + 1] = (prefix[i] - (prefix[j - 1] if j > 0 else 0)) % mod
+    prefix[i + 1] = (prefix[i] + dp[i + 1]) % mod
+
+  return dp[n]
+
+if __name__ == '__main__':
+  main()
