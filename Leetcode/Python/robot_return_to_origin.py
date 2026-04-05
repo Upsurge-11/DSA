289b997422eaa8b1fba41086a7aa212cfdd1e685
Arghyadeep Mukherjee <arghyadeepbumbum@gmail.com>
2026-04-06 01:04:02 +0530

    Add implementation for robot return to origin problem

---
+++ Leetcode/Python/robot_return_to_origin.py
@@ -0,0 +1,20 @@
+def main():
+  moves = input()
+  print(judgeCircle(moves))
+
+def judgeCircle(moves: str) -> bool:
+  x = 0
+  y = 0
+  for move in moves:
+    if move == 'U':
+      y += 1
+    elif move == 'D':
+      y -= 1
+    elif move == 'L':
+      x -= 1
+    elif move == 'R':
+      x += 1
+  return x == 0 and y == 0
+
+if __name__ == '__main__':
+  main()
